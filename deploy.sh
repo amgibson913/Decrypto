388ddec20fcae8e0db0ce0d20367383e9c5ff29b
AGibson <amgibson913@gmail.com>
2021-02-03 15:06:30 -0500

    added deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
+git pull
+
 docker build -t decryptofrontend:latest ./frontend
 docker build -t decryptobackend:latest ./backend
+
+docker run --name backend -d -p 5000:5000 --rm decryptobackend:latest
+docker run --name frontend -d -p 8080:80 --rm decryptofrontend:latest

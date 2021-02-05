41ae5ddb6a065d1f0dd0700a58a571bd4ebcdfaa
AGibson <amgibson913@gmail.com>
2021-02-04 21:16:39 -0500

    cors

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['SESSION_TYPE'] = 'filesystem'
 
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 games = {}  # Holds all game objects
 players = {}  # Holds players and their corresponding room, for disconnects
 

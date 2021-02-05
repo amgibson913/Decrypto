86b34c274cf37bc6521ee15d95f8db3440d6e7eb
AGibson <amgibson913@gmail.com>
2021-02-04 21:24:55 -0500

    boot.sh

--- backend/boot.sh
+++ backend/boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source venv/bin/activate
-exec gunicorn -b :5000 --access-logfile - --error-logfile - app:app
+exec gunicorn --worker-class eventlet -w 1 -b :5000 --access-logfile - --error-logfile - app:app
